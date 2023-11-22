0340c9c945427f2c853210dff0a4ab2320e4eb1f
DasigoSZK <dasigoszkamarda2@hotmail.com>
2023-11-22 17:36:01 -0300

    home slider (javascript)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 import hamburgerMenu from "./burger_menu.js";
+import { autoSlider } from "./slider.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".panel-btn", ".panel", ".menu");
+  autoSlider();
 })
