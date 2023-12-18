6ebb3bcccf206d285ca96ed54510f281f744fdae
DasigoSZK <dasigoszkamarda2@hotmail.com>
2023-12-18 18:26:14 -0300

    about spaceship move function

--- js/index.js
+++ js/index.js
@@ -2,12 +2,15 @@ import hamburgerMenu from "./burger_menu.js";
 import { autoSlider } from "./slider.js";
 import { switchProject } from "./switch_project.js";
 import { contactFormValidations } from "./contact_form.js";
+import { moveSpacehsip } from "./move_spaceship.js";
 
 const d = document;
+let { hash } = location;
 
 d.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".panel-btn", ".panel", ".menu");
-  autoSlider();
-  switchProject();
-  contactFormValidations();
+  //autoSlider();
+  //switchProject();
+  //contactFormValidations();
+  moveSpacehsip(".skills", ".icons_spaceship");
 })
