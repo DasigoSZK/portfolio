6d4a3eda48af6df75c10d9ae1ea7179542c82b0b
DasigoSZK <dasigoszkamarda2@hotmail.com>
2023-12-27 19:29:50 -0300

    MPA version of the project deleted

--- mpa/js/contact.js
+++
@@ -1,9 +0,0 @@
-import hamburgerMenu from "./burger_menu.js";
-import { contactFormValidations } from "./contact_form.js";
-
-const d = document;
-
-d.addEventListener("DOMContentLoaded", (e) => {
-  hamburgerMenu(".panel-btn", ".panel", ".menu");
-  contactFormValidations();
-})
--- mpa/js/work.js
+++
@@ -1,9 +0,0 @@
-import hamburgerMenu from "./burger_menu.js";
-import { btnSound } from "./btn_sound.js";
-
-const d = document;
-
-d.addEventListener("DOMContentLoaded", (e) => {
-  hamburgerMenu(".panel-btn", ".panel", ".menu");
-  btnSound(".hero-btn");
-})
